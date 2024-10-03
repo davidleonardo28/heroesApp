fe90aa44778ee83b1a604dd95f0e28314f948e3b
David Barrera <store.things975@gmail.com>
2024-10-03 16:48:25 -0500

    Db config commit initial

---
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginPageComponent {}
