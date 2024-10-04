75790202494dd1d8e1ee7fd09293ae6b703b25ad
David Barrera <store.things975@gmail.com>
2024-10-03 22:17:47 -0500

    Db Backend Config

--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
+import { HttpClient } from '@angular/common/http';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-list-page',
   templateUrl: './list-page.component.html',
-  styles: ``
+  styles: ``,
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit {
+  heroes: Hero[] = [];
 
+  constructor(private heroesService: HeroesService) {}
+
+  ngOnInit(): void {
+    this.heroesService
+      .getHeroes()
+      .subscribe((heroes) => (this.heroes = heroes));
+  }
 }
---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,3 @@
+export const environments = {
+  baseUrl: 'https://heroes-app.com/api',
+};
